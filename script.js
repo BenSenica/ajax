552085cc91e7a2e2eb19ac272c011ef5fde4a3fc
Ben Senica <senicaben@gmail.com>
2019-07-31 15:07:36 +0200

    added code to request and receive the quote from webserver

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.getElementById("button1").addEventListener("click", loadQuote);
 
 function loadQuote() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://thatsthespir.it/api", true);
 
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var quote = JSON.parse(this.responseText);
+
+        }
+        console.log(quote);
+    }
+    xhr.send();
 }
